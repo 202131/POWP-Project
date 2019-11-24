da7c4f126f4cace30988b96000b09e7c56f7f27f
unknown <Student@L404-15.carbon.cti.p.lodz.pl>
2019-11-24 16:11:39 +0100

    Added two types of waffles to waffle folder with price in there and description

---
+++ src/production/WaffleShop/Waffle/CircleWaffle.java
@@ -0,0 +1,11 @@
+package production.WaffleShop.Waffle;
+
+public class CircleWaffle extends Waffle {
+    public CircleWaffle() {
+        this.description = "Circle Waffle";
+    }
+
+    public double cost() {
+        return 4.0D;
+    }
+}
---
+++ src/production/WaffleShop/Waffle/SquareWaffle.java
@@ -0,0 +1,11 @@
+package production.WaffleShop.Waffle;
+
+public class SquareWaffle extends Waffle {
+    public SquareWaffle() {
+        this.description = "Square Waffle";
+    }
+
+    public double cost() {
+        return 5.0D;
+    }
+}
